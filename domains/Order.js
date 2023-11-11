def49ee7753c879a57ce197b537bec7e663a99f5
고세종 <fulfilled7418@gmail.com>
2023-11-11 22:21:11 +0900

    feat: 구현 주문 객체 Order 클래스

---
+++ domains/Order.js
@@ -0,0 +1,24 @@
+import OrderValidation from './OrderValidation';
+
+class Order {
+  #menu;
+
+  constructor(menu) {
+    this.#validate(menu);
+    this.#menu = menu;
+  }
+
+  #validate(menu) {
+    const orderValidation = new OrderValidation(menu);
+    if (
+      orderValidation.checkInMenu() ||
+      orderValidation.checkIsMenuCountValid() ||
+      orderValidation.checkIsMenuNonDuplicated() ||
+      orderValidation.checkIsTotalCountValid()
+    ) {
+      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+    }
+  }
+}
+
+export default Order;
