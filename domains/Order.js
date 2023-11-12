5dfcfafb231e744d509acb66167de161fd7d6e48
고세종 <fulfilled7418@gmail.com>
2023-11-12 14:39:18 +0900

    fix: 수정 에러메세지

--- domains/Order.js
+++ domains/Order.js
@@ -17,7 +17,7 @@ class Order {
       orderValidation.checkIsTotalCountValid() ||
       orderValidation.checkIsAllBeverage()
     ) {
-      throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+      throw new Error('[ERROR]');
     }
   }
 }
