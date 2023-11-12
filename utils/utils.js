0a426d7fe5e6ba7449a38eb4b3b2e832c8a470a9
고세종 <fulfilled7418@gmail.com>
2023-11-12 13:31:08 +0900

    feat: 구현 객체의 특정 key를 기준으로 비교하는 기능

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,13 @@
-const extractKeys = (arr, keyName) => arr.map((item) => item[keyName]);
+const extractKeys = (arr, baseKeyName, targetKeyName) => {
+  if (targetKeyName) {
+    const returnArr = arr.reduce((acc, cur) => {
+      if (cur.type === targetKeyName) acc.push(cur[baseKeyName]);
+      return acc;
+    }, []);
+
+    return returnArr;
+  }
+  return arr.map((item) => item[baseKeyName]);
+};
 
 export { extractKeys };
