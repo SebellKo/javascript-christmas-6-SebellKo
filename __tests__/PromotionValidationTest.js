582ce64f24cd34aa2206bf5082324cef3e464383
고세종 <fulfilled7418@gmail.com>
2023-11-12 23:14:09 +0900

    remove: 제거 파일이름 변경에 의한

--- __tests__/PromotionValidationTest.js
+++
@@ -1,8 +0,0 @@
-import Promotion from '../domains/Promotion';
-
-describe('프로모션 날짜가 12월인지 확인.', () => {
-  const dates = [0, 32];
-  test.each(dates)('InValid 테스트', (input) => {
-    expect(() => new Promotion(input)).toThrow('[ERROR]');
-  });
-});
