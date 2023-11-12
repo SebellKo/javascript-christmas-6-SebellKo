7edbed18b0f05eae19b5aa66f83b833936c38e8c
고세종 <fulfilled7418@gmail.com>
2023-11-12 23:12:23 +0900

    feat: 구현 이벤트 진행 도메인 로직 Promotion 클래스

--- domains/Promotion.js
+++ domains/Promotion.js
@@ -1,19 +1,36 @@
-import { checkIsInRange } from '../utils/ValidationUtils.js';
+import Order from './Order';
+import Discount from './Discount';
 
 class Promotion {
-  #date;
+  #order;
+  #discount;
 
-  constructor(date) {
-    this.#validate(date);
-    this.#date = date;
+  constructor(menu, date) {
+    this.#order = new Order(menu);
+    this.#discount = new Discount(date);
   }
 
-  #validate(date) {
-    if (!checkIsInRange(1, 31, date)) throw new Error('[ERROR]');
+  checkCanGetGift() {
+    const totalPrice = this.#order.calculateTotalPrice();
+    return totalPrice >= 120000;
+  }
+
+  calculateTotalDiscount() {
+    const discountList = this.#discount.checkDate();
+    const orderBoard = this.#order.createOrderBoard();
+    let totalDiscount = 0;
+
+    if (discountList?.christmasDday)
+      totalDiscount += this.#discount.calculateChristmasDdayDiscount();
+    if (discountList?.weekend)
+      totalDiscount += this.#discount.calculateWeekendDiscount(orderBoard);
+    if (discountList?.weekday)
+      totalDiscount += this.#discount.calculateWeekdayDiscount(orderBoard);
+    if (discountList?.starDay)
+      totalDiscount += this.#discount.calculateStarDayDiscount();
+
+    return totalDiscount;
   }
 }
-
-const date = new Promotion(13);
-console.log(date);
 
 export default Promotion;
