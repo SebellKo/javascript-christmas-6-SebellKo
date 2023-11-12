6da14548dad5c2b3db7ae83e8527b5651088b524
고세종 <fulfilled7418@gmail.com>
2023-11-12 13:33:02 +0900

    test: 구현 주문 메뉴가 전부 음료인지 검사

--- __tests__/OrderValidationTest.js
+++ __tests__/OrderValidationTest.js
@@ -1,6 +1,7 @@
 import OrderValidation from '../domains/OrderValidation';
 
 describe('Order 클래스 유효성 테스트', () => {
+  let orderValidation = 0;
   describe('Valid 테스트', () => {
     const expectedResult = true;
     const validMenu = [
@@ -9,10 +10,45 @@ describe('Order 클래스 유효성 테스트', () => {
       { menu: '초코케이크', count: 2 },
       { menu: '제로콜라', count: 1 },
     ];
-    let orderValidation = 0;
 
     beforeEach(() => {
       orderValidation = new OrderValidation(validMenu);
+    });
+
+    test('checkInMenu 테스트', () => {
+      const testResult = orderValidation.checkInMenu();
+      expect(testResult).toEqual(expectedResult);
+    });
+
+    test('checkIsMenuCountValid 테스트', () => {
+      const testResult = orderValidation.checkIsMenuCountValid();
+      expect(testResult).toEqual(expectedResult);
+    });
+
+    test('checkIsMenuNonDuplicated 테스트', () => {
+      const testResult = orderValidation.checkIsMenuNonDuplicated();
+      expect(testResult).toEqual(expectedResult);
+    });
+
+    test('checkIsTotalCountValid 테스트', () => {
+      const testResult = orderValidation.checkIsTotalCountValid();
+      expect(testResult).toEqual(expectedResult);
+    });
+  });
+
+  describe('InValid 테스트', () => {
+    const expectedResult = false;
+    const inValidMenu = [
+      { menu: '양송이스프', count: 0 },
+      { menu: '바비큐립', count: 1 },
+      { menu: '생크림케이크', count: 2 },
+      { menu: '제로콜라', count: 1 },
+      { menu: '제로콜라', count: 1 },
+      { menu: '시저샐러드', count: 19 },
+    ];
+
+    beforeEach(() => {
+      orderValidation = new OrderValidation(inValidMenu);
     });
 
     test('checkInMenu 테스트', () => {
@@ -36,39 +72,26 @@ describe('Order 클래스 유효성 테스트', () => {
     });
   });
 
-  describe('InValid 테스트', () => {
-    const expectedResult = false;
-    const inValidMenu = [
-      { menu: '양송이스프', count: 0 },
-      { menu: '바비큐립', count: 1 },
-      { menu: '생크림케이크', count: 2 },
+  describe('메뉴가 모두 음료인지 확인 테스트. (checkIsAllBeverage 메소드 테스트)', () => {
+    const InValidMenu = [
       { menu: '제로콜라', count: 1 },
-      { menu: '제로콜라', count: 1 },
-      { menu: '시저샐러드', count: 19 },
+      { menu: '레드와인', count: 1 },
     ];
-    let orderValidation = 0;
-    beforeEach(() => {
-      orderValidation = new OrderValidation(inValidMenu);
+    const validMenu = [
+      { menu: '바비큐립', count: 1 },
+      { menu: '제로콜라', count: 1 },
+    ];
+
+    test('InValid (모든 메뉴가 음료)', () => {
+      orderValidation = new OrderValidation(InValidMenu);
+      const testResult = orderValidation.checkIsAllBeverage();
+      expect(testResult).toEqual(true);
     });
 
-    test('checkInMenu 테스트', () => {
-      const testResult = orderValidation.checkInMenu();
-      expect(testResult).toEqual(expectedResult);
-    });
-
-    test('checkIsMenuCountValid 테스트', () => {
-      const testResult = orderValidation.checkIsMenuCountValid();
-      expect(testResult).toEqual(expectedResult);
-    });
-
-    test('checkIsMenuDuplicated 테스트', () => {
-      const testResult = orderValidation.checkIsMenuNonDuplicated();
-      expect(testResult).toEqual(expectedResult);
-    });
-
-    test('checkIsTotalCountValid 테스트', () => {
-      const testResult = orderValidation.checkIsTotalCountValid();
-      expect(testResult).toEqual(expectedResult);
+    test('Valid (메인 메뉴가 포함)', () => {
+      orderValidation = new OrderValidation(validMenu);
+      const testResult = orderValidation.checkIsAllBeverage();
+      expect(testResult).toEqual(false);
     });
   });
 });
