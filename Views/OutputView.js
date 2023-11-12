9b44f5f3535c69a1744f96fcf3d276015fa4ce32
고세종 <fulfilled7418@gmail.com>
2023-11-12 12:13:00 +0900

    feat: 구현 유효하지 않은 날짜, 메뉴 에러 출력

--- Views/OutputView.js
+++ Views/OutputView.js
@@ -1,9 +1,17 @@
+import { Console } from '@woowacourse/mission-utils';
+
 const OutputView = {
   printMenu() {
     Console.print('<주문 메뉴>');
-    // ...
   },
-  // ...
+
+  printDateError() {
+    Console.print('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+  },
+
+  printMenuError() {
+    Console.print('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+  },
 };
 
 export default OutputView;
