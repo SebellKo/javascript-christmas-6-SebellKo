3a7d1a4beca7adf7db972f5494bf2bb889357166
고세종 <fulfilled7418@gmail.com>
2023-11-12 12:54:28 +0900

    feat: 구현 사용자 입력 유효성 검사

--- Views/InputView.js
+++ Views/InputView.js
@@ -1,19 +1,52 @@
 import { Console } from '@woowacourse/mission-utils';
+import {
+  checkIsNumber,
+  checkIsKorean,
+  checkIncludeHypen,
+} from '../utils/ValidationUtils.js';
+import OutputView from './OutputView.js';
 
 const InputView = {
   async readDate() {
-    const input = await Console.readLineAsync(
+    let date = await Console.readLineAsync(
       '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n',
     );
 
-    return input;
+    while (!checkIsDateNumber(date)) {
+      OutputView.printDateError();
+      date = await InputView.readDate();
+    }
+
+    return Number(date);
   },
+
   async readOrder() {
-    const input = await Console.readLineAsync(
-      '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
+    let order = await Console.readLineAsync(
+      '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n',
     );
 
-    return input;
+    while (!checkIsOrderInForm(order)) {
+      OutputView.printMenuError();
+      order = await InputView.readOrder();
+    }
+
+    return order.split(',');
+  },
+
+  checkIsDateNumber(date) {
+    return checkIsNumber(date);
+  },
+
+  checkIsOrderInForm(order) {
+    const checkHypen = order.split(',');
+    const checkReg = checkHypen.map((menu) => menu.split('-'));
+
+    return (
+      checkHypen.every((menu) => checkIncludeHypen(menu)) &&
+      checkReg.every(
+        (index) => checkIsKorean(index[0]) && checkIsNumber(index[1]),
+      )
+    );
   },
 };
 
