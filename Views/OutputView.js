0986979383eb1b3b16d9bdaaf33b3312ebcfcd49
고세종 <fulfilled7418@gmail.com>
2023-11-14 23:34:34 +0900

    fix: 수정 금액 출력 스트링 변환, 에러 출력 리팩터링

--- Views/OutputView.js
+++ Views/OutputView.js
@@ -4,49 +4,62 @@ const OutputView = {
   printOrderMenu(menu) {
     Console.print('<주문 메뉴>');
     menu.forEach((menu) => {
-      Console.print(`${menu.menu} ${menu.count}개`);
+      Console.print(`${menu.name} ${menu.amount}개`);
     });
+    Console.print('');
   },
 
-  printDateError() {
-    Console.print('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+  printStartMent() {
+    Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
   },
 
-  printMenuError() {
-    Console.print('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+  printError(error) {
+    Console.print(`[ERROR] 유효하지 않은 ${error}입니다. 다시 입력해 주세요.`);
   },
 
   printVisitDate(date) {
     Console.print(
-      `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`,
+      `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`,
     );
   },
 
   printTotalPrcie(price) {
     Console.print('<할인 전 총주문 금액>');
-    Console.print(`${price}원`);
+    Console.print(`${price.toLocaleString('ko-KR')}원`);
+    Console.print('');
   },
 
-  printGift() {
+  printGift(gift) {
     Console.print('<증정 메뉴>');
-    Console.print('샴페인 1개');
+    Console.print(gift);
+    Console.print('');
   },
 
   printPromotion(promotion) {
     Console.print('<혜택 내역>');
-    promotion.forEach((item) => {
-      if (item.price !== 0) Console.print(`${itme.name} -${item.price}원`);
-    });
+    if (Object.keys(promotion).length === 0) {
+      Console.print('없음');
+      Console.print('');
+      return;
+    }
+    Object.entries(promotion).forEach((item) =>
+      Console.print(`${item[0]}: -${item[1].toLocaleString('ko-KR')}원`),
+    );
+    Console.print('');
   },
 
-  printTotalPromotionPrice(price) {
+  printTotalDiscount(price) {
+    const totalDiscount =
+      price === 0 ? price : `-${price.toLocaleString('ko-KR')}`;
     Console.print('<총혜택 금액>');
-    Console.print(`-${price}원`);
+    Console.print(`${totalDiscount}원`);
+    Console.print('');
   },
 
-  printPrcieForPay(price) {
+  printPriceForPay(price) {
     Console.print('<할인 후 예상 결제 금액>');
-    Console.print(`${price}원`);
+    Console.print(`${price.toLocaleString('ko-KR')}원`);
+    Console.print('');
   },
 
   printEventBadge(badge) {
@@ -59,7 +72,7 @@ const OutputView = {
   },
 
   printNewLine() {
-    Console.print('\n');
+    Console.print('');
   },
 };
 
